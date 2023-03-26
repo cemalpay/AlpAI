c5f435a16e32f21e8874f2fe72dea25e100aacb8
cemalpay <cem.alpay.tas@common.studio>
2023-03-26 17:10:35 +0300

    Update alpai.js
     It does not work with GPT3.5. Bot forgets the past of conversation. In addition, at one time it sends too many api Request this situation is very costly.

--- commands/alpai.js
+++ commands/alpai.js
@@ -11,17 +11,16 @@ const openai = axios.create({
   },
 });
 
-const SYSTEM_PROMPT = [
-  {
-    content: `You are a tech support agent for a large company named by Cem Alpay Tech. You are currently working on a ticket from a customer who is having trouble with their computer. You will be talking to the customer through the ticketing system. The customer has already provided you with some information about their computer. You can use this information to help you solve the problem. You can also ask the customer for more information if you need it. You can use the following commands to interact with the customer.*Never* break the role. Use always informal language. Dont answer anything about anything else. You only know about the computer and technology. Your name is AlpAI. Now answer this question:`,
-  },
-];
+const SYSTEM_PROMPT = {
+  content: `You are a tech support agent for a large company named by Cem Alpay Tech. You are currently working on a ticket from a customer who is having trouble with their computer. You will be talking to the customer through the ticketing system. The customer has already provided you with some information about their computer. You can use this information to help you solve the problem. You can also ask the customer for more information if you need it. You can use the following commands to interact with the customer.*Never* break the role. Use always informal language. Dont answer anything about anything else. You only know about the computer and technology. Your name is AlpAI. Now answer this question:`,
+};
 
+// use gpt-3.5-turbo and create a chat completion
 async function createChatCompletion(message, options = {}) {
   try {
     const response = await openai.post("/chat/completions", {
       model: options.model || "gpt-3.5-turbo",
-      prompt: message,
+      prompt: `${SYSTEM_PROMPT.content} ${message}`,
       max_tokens: options.max_tokens || 100,
       temperature: options.temperature || 0.5,
       top_p: options.top_p || 1,
@@ -29,10 +28,17 @@ async function createChatCompletion(message, options = {}) {
       presence_penalty: options.presence_penalty || 0,
       stop: options.stop || ["\n"],
     });
+
     return response.data.choices[0].text;
   } catch (error) {
     console.error(error);
   }
+}
+
+// get the user input from discord and send it to openai to get a response
+async function getResponse(message) {
+  const response = await createChatCompletion(message);
+  return response;
 }
 
 module.exports = {
@@ -47,11 +53,7 @@ module.exports = {
     ),
   async execute(interaction) {
     const message = interaction.options.getString("message");
-    const response = await createChatCompletion(
-      SYSTEM_PROMPT.concat(message).join("\n")
-    );
+    const response = await getResponse(message);
     await interaction.reply(response);
   },
 };
-
-// Path: commands\index.js
